54f612f544b39438ab8f6c91d3736398d0239de4
wailovet <wailovet@163.com>
2017-06-16 16:14:00 +0800

    change

--- app.php
+++ app.php
@@ -3,8 +3,14 @@ use Nsio\Io;
 
 require_once __DIR__ . "/./autoload.php";
 
-$server = Server::createHttp(80);
-$server->connection(function (Io $io) {
+$http_server = Server::createHttp(1083);
+$http_server->connection(function (Io $io) {
     $io->emit("message", "开始");
 });
 
+$websocket = Server::createWebsocket(1084);
+$websocket->connection(function (Io $io) {
+    $io->on("hi", function ($data) use ($io) {
+        $io->emit("hi", $data);
+    });
+});
